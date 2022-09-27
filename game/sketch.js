8987e4cbfbaeec0397861313a4c1dc2c3c544f17
moky621 <114016129+moky621@users.noreply.github.com>
2022-09-27 12:12:08 -0600

    new features

--- game/sketch.js
+++ game/sketch.js
@@ -9,14 +9,16 @@
 let x = 100;
 let y = 100;
 let charSpeed = 4;
-let img;
-let img1;
-
+let img, img1, img2;
+let laserx = 1500;
+let someTime = 2000;
+let shoot = true;
 
 function setup() {
   createCanvas(1600, 800);
   img = loadImage("robber.png");
   img1 = loadImage("laser.png");
+  img2 = loadImage("money.png");
 }
 
 function draw() {
@@ -25,14 +27,33 @@ function draw() {
   keyTyped();
   //setInterval(laser(), 3000)
   laser();
+  money();
+  idk();
 }
- 
+
+function idk() {
+  if (shoot) {
+    let lasery = 500;
+    image(img1, laserx, lasery, 200, 100);
+    laserx -= 5;
+  }
+  if (millis > someTime) {
+    shoot = !shoot;
+    someTime = millis() + 2000;
+  }
+}
 
 function robber(){
   image(img, x, y, 300, 200);
 }
+
+function money(){
+  let x = width/2;
+  let y = height/2;
+  image(img2, x, y, 100, 50);
+}
+
 function laser(){
-  let laserx = 1500;
   let lasery = 500;
   image(img1, laserx, lasery, 200, 100);
   laserx -= 5;
