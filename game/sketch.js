22cd3f0c18f38efeff9cec3989b0c768e5d2305f
moky621 <114016129+moky621@users.noreply.github.com>
2022-10-05 12:05:47 -0600

    gotham!

--- game/sketch.js
+++ game/sketch.js
@@ -10,19 +10,25 @@ let x = 100;
 let y = 100;
 let charSpeed = 4;
 let img, img1, img2;
+let bg;
 let laserx = 1500;
 let someTime = 0;
 let shoot = "on";
 let lasery;
-let hit;
+let hit, cash;
 let state = "real";
+let moneyX, moneyY;
 
+function preload(){
+  bg = loadImage("gotham.jpg"); 
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
   img = loadImage("robber.png");
   img1 = loadImage("laser.png");
   img2 = loadImage("money.png");
 }
+
 
 
 function draw() {
@@ -33,17 +39,22 @@ function draw() {
     money();
     beam();
     laser();
+    // back();
     // detectHit();
   }
 }
-
+function back(){
+  imageMode(CENTER);
+  imageMode(back);
+  image(bg, windowWidth/2, windowHeight/2);
+}
 
 
 function beam() {
   if (millis() >= 2000+someTime) {
     laser();
     someTime = millis();
-  
+    
   }
 }
 
@@ -58,27 +69,37 @@ function beam() {
 //     }
 //   }
 // }
-
+      
 function robber(){
   image(img, x, y, 300, 200);
 }
-
-function detectHit(){
-
-  hit = collideRectRect(x, y, 300, 200, laserx, lasery, 200, 100);
-  if (hit){
+      
+function detectHit(){ 
+  let hit = collideRectRect(x, y, 300, 200, laserx, lasery, 200, 100);
+  if (!hit){
+    state = "real";
+  }
+  else{
     state = "dead";
   }
 }
-function money(){
-  let x = width/2;
-  let y = height/2;
-  image(img2, x, y, 100, 50);
-}
 
+function moneyGrab(){
+  let cash = collideRectRect(x, y, 300, 200, moneyX, moneyY, 100, 50);
+  if (cash){
+    moneyX = random(0, 1000);
+    moneyY = random(0, 800);
+  }
+}
+function money(){
+  moneyX = width/2;
+  moneyY = height/2;
+  image(img2, moneyX, moneyY, 100, 50);
+}
+      
 function laser(){
   image(img1, laserx, lasery, 200, 100);
-  laserx -= 10;
+  laserx -= 30;
   if (laserx < -100){
     laserx = 1500;
     lasery = random(0, 800);
