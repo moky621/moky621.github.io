a2632cd5c2a278f1e5da64272c2458e5f8333332
moky621 <114016129+moky621@users.noreply.github.com>
2022-11-01 12:14:42 -0600

    f

--- 2d array game init/sketch.js
+++ 2d array game init/sketch.js
@@ -5,15 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const ROWS = 40;
-const COLS = 40;
+const ROWS = 20;
+const COLS = 30;
+let cellHeight;
+let cellWidth;
+let char;
+let playerX, playerY;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellWidth = width/COLS;
+  cellHeight = height/ROWS;
+  grid = create2dArray(COLS, ROWS);
+  char = loadImage("default.png");
+  grid[playerY][playerX] = 9;
 }
 
 function draw() {
   background(220);
+  displayGrid(grid);
+  player();
 }
 
 function create2dArray(COLS, ROWS) {
@@ -25,4 +37,22 @@ function create2dArray(COLS, ROWS) {
     }
   }
   return emptyArray;
+}
+
+function player(){
+  image(char, 50, 50, 50 ,50)
+}
+function displayGrid(grid) {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
+      if (grid[y][x] === 0) {
+        fill("black");
+        stroke("blue");
+      }
+      else if(grid[y][x] === 9) {
+        fill("red");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
 }
