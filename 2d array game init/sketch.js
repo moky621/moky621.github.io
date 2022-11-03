9f65f9f71ea7aa8dfcc6ff458e387c48ae466eb8
moky621 <114016129+moky621@users.noreply.github.com>
2022-11-03 11:15:03 -0600

    dfsf

--- 2d array game init/sketch.js
+++ 2d array game init/sketch.js
@@ -5,23 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const ROWS = 30;
-const COLS = 30;
+const ROWS = 10;
+const COLS = 20;
 let grid;
 let cellWidth;
 let cellHeight;
 let playerX = 0;
 let playerY = 0;
-let character;
+let character, power;
+let stoneX = 0;
+let stoneY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellWidth = width/COLS;
   cellHeight = height/ROWS;
   grid = create2dArray(COLS, ROWS);
-  character = loadImage("default.png");
+  character = loadImage("thanos.png");
+  power = loadImage("power.jpg");
   //place player in grid
   grid[playerY][playerX] = 9;
+  grid[stoneY][stoneX] = 8;
 }
 
 function draw() {
@@ -58,6 +62,55 @@ function displayGrid(grid) {
         
         image(character, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
+      else if (grid[y][x] === 8) {
+        image(power, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+    }
+  }
+}
+
+function keyTyped(){
+  if (grid[playerY][playerX + 1] === 0) {
+    if (key === 'd') {
+    // reset old loaction to white
+      grid[playerY][playerX] = 0;
+      //move
+      playerX++;
+      //new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (grid[playerY][playerX - 1] === 0) {
+    if (key === 'a') {
+    // reset old loaction to white
+      grid[playerY][playerX] = 0;
+      //move
+      playerX--;
+      //new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (grid[playerY+1][playerX] === 0) {
+    if (key === 's') {
+    // reset old loaction to white
+      grid[playerY][playerX] = 0;
+      //move
+      playerY++;
+      //new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (grid[playerY-1][playerX] === 0) {
+    if (key === 'w') {
+    // reset old loaction to white
+      grid[playerY][playerX] = 0;
+      //move
+      playerY--;
+      //new player location
+      grid[playerY][playerX] = 9;
     }
   }
 }
