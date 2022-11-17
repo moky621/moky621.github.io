636d021729ee6d4ca67e80b18ab71fa90da94a75
Etagiuri5104568 <5104568@sps-student.sk.ca>
2022-11-17 12:16:22 -0600

    bullet

--- bullet/sketch.js
+++ bullet/sketch.js
@@ -7,23 +7,34 @@
 
 
 class Bullet {
-  constructor(x, y) {
+  constructor(x, y, theImage) {
     this.x = x;
     this.y = y;
     this.radius = 5;
     this.dx = 10;
+    this.image = theImage;
   }
   move(){
     this.x += this.dx;
   }
   display() {
-    fill("black");
-    circle(this.x, this.y, this.radius*2);
+    // fill("black");
+    // circle(this.x, this.y, this.radius*2);
+    image(this.image, this.x, this.y, this.image.width*0.3, this.image.height*0.3);
+  }
+
+  isDead() {
+    return this.x <= width;
   }
  
 }
 
 let bulletArray = [];
+let bulletImg;
+
+function preload() {
+  bulletImg = loadImage("bullet.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -33,14 +44,17 @@ function draw() {
   background(220);
   for (let i = 0; i < bulletArray.length; i++){
     bulletArray[i].move();
-   
+    
     bulletArray[i].display();
+    // if (bulletArray[i].isDead) {
+    //   bulletArray.splice(i, 1);
+    // }
     
   }
-
+  
 }
 
-function mousePressed(){
-  let pro = new Bullet(0, random(height));
+function keyPressed(){
+  let pro = new Bullet(0, random(height), bulletImg);
   bulletArray.push(pro);
 }
